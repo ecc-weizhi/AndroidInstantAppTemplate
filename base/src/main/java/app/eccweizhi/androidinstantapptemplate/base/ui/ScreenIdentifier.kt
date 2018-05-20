661b3bc06b22fe7f024b365ba97e81f992f222c7
Lin Weizhi <ecc.weizhi@gmail.com>
2018-05-20 12:54:00 +0800

    Change to use subdomain androidinstantapptemplate.eccweizhi.app

--- base/src/main/java/app/eccweizhi/androidinstantapptemplate/base/ui/ScreenIdentifier.kt
+++ base/src/main/java/app/eccweizhi/androidinstantapptemplate/base/ui/ScreenIdentifier.kt
@@ -4,9 +4,9 @@ package app.eccweizhi.androidinstantapptemplate.base.ui
 class ScreenIdentifier {
     companion object {
         private const val SCHEME = "https://"
-        private const val HOST = "eccweizhi.app"
+        private const val HOST = "androidinstantapptemplate.eccweizhi.app"
 
-        const val URI_FEATURE_ONE = "$SCHEME$HOST/androidinstantapptemplate/featureone"
-        const val URI_FEATURE_TWO = "$SCHEME$HOST/androidinstantapptemplate/featuretwo"
+        const val URI_FEATURE_ONE = "$SCHEME$HOST/featureone"
+        const val URI_FEATURE_TWO = "$SCHEME$HOST/featuretwo"
     }
 }

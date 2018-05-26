60fa129da5f4130c363c98f86009dc275c1a860c
Lin Weizhi <ecc.weizhi@gmail.com>
2018-05-26 20:27:43 +0800

    Added winter module

---
+++ winter/src/main/java/app/eccweizhi/androidinstantapptemplate/winter/ui/WinterActivity.kt
@@ -0,0 +1,14 @@
+package app.eccweizhi.androidinstantapptemplate.winter.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import app.eccweizhi.androidinstantapptemplate.base.ui.main.MainActivity
+
+
+class WinterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MainActivity.startWith(this,
+                WinterFragment.WinterKey("foo"))
+    }
+}
---
+++ winter/src/main/java/app/eccweizhi/androidinstantapptemplate/winter/ui/WinterFragment.kt
@@ -0,0 +1,65 @@
+package app.eccweizhi.androidinstantapptemplate.winter.ui
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.eccweizhi.androidinstantapptemplate.base.ui.BaseFragment
+import app.eccweizhi.androidinstantapptemplate.base.ui.BaseKey
+import app.eccweizhi.androidinstantapptemplate.base.ui.FragmentListener
+import app.eccweizhi.androidinstantapptemplate.winter.R
+import kotlinx.android.parcel.Parcelize
+
+
+class WinterFragment : BaseFragment() {
+    var fragmentListener: FragmentListener? = null
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_winter, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            fragmentListener = context as FragmentListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException("$context must implement FragmentListener")
+        }
+    }
+
+    override fun onDetach() {
+        fragmentListener = null
+        super.onDetach()
+    }
+
+    companion object {
+        const val FRAGMENT_TAG = "WinterFragment"
+        private const val ARG_PARAM_1 = "param1"
+
+        @JvmStatic
+        fun newInstance(param1: String): WinterFragment {
+            val frag = WinterFragment()
+            val args = Bundle().apply {
+                putString(ARG_PARAM_1, param1)
+            }
+            frag.arguments = args
+            return frag
+        }
+    }
+
+    @Parcelize
+    data class WinterKey(val clazz: String,
+                         val param1: String) : BaseKey() {
+        constructor(param1: String) : this(WinterFragment.FRAGMENT_TAG, param1)
+
+        override fun createFragment(): Fragment = WinterFragment.newInstance(param1)
+    }
+}

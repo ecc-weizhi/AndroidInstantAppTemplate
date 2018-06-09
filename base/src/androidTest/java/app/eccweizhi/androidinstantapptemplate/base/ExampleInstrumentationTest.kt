863e2af3a6d6fb261f6de175e9ad951be3e41e03
Lin Weizhi <ecc.weizhi@gmail.com>
2018-06-09 17:39:23 +0800

    Enable multidex for debug build because i don't know how to apply proguard to test. Add example tests

---
+++ base/src/androidTest/java/app/eccweizhi/androidinstantapptemplate/base/ExampleInstrumentationTest.kt
@@ -0,0 +1,24 @@
+package app.eccweizhi.androidinstantapptemplate.base
+
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("app.eccweizhi.androidinstantapptemplate.base.test", appContext.packageName)
+    }
+}
---
+++ base/src/test/java/app/eccweizhi/androidinstantapptemplate/base/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package app.eccweizhi.androidinstantapptemplate.base
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
